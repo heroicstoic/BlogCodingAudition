d7b2c2054025823f3aba739dd22a6029b12e1afc
heroicstoic <cinnaman.123@gmail.com>
2018-02-27 11:08:39 -0600

    Boilerplate created

--- webproject/webproject/urls.py
+++ webproject/webproject/urls.py
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import include
+
+urlpatterns += [
+    path('blog/', include('blog.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/blog/')),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
