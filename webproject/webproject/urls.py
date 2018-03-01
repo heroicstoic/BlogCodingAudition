46944c0e7f318f3126a984d45ab88870ff23d2de
heroicstoic <cinnaman.123@gmail.com>
2018-03-01 09:36:16 -0600

    slight cleanup, starting register link

--- webproject/webproject/urls.py
+++ webproject/webproject/urls.py
@@ -14,28 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.urls import include
-
-urlpatterns += [
-    path('blog/', include('blog.urls')),
-]
-
+from django.urls import path, include
 from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='/blog/')),
-]
-
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
+urlpatterns = [
+    path('', RedirectView.as_view(url='/blog/')),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
