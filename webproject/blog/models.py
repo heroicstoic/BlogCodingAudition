7f9a6c17bfae289fff102a464bb0f0e1a9aff366
heroicstoic <cinnaman.123@gmail.com>
2018-03-01 16:24:21 -0600

    Add Blog model

--- webproject/blog/models.py
+++ webproject/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length = 20, help_text = "Title of the blog")
+	content = models.TextField(help_text = "Content of the blog")
+	datePublished = models.DateTimeField(auto_now = True, help_text = "Date blog published")
+
+	class Meta:
+		ordering = ["datePublished", "-title", "-content"]
+
+	def get_absolute_url(self):
+		return reverse('model-detail-view', args=[str(self.id)])
+
+	def __str__(self):
+		return self.title
