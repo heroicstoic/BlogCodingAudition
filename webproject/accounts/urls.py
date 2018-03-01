ecb879993074ca32c48b228d435bdb6debf1e5e5
heroicstoic <cinnaman.123@gmail.com>
2018-03-01 14:14:58 -0600

    Added registration form

--- webproject/accounts/urls.py
+++ webproject/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
 	path('', login, {'template_name': 'login.html'}),
 	path('login/', login, {'template_name': 'login.html'}, name='login'),
+	path('logout/', logout, {'template_name': 'logout.html'}, name='logout'),
+	path('register/', views.register, name='register'),
 ]
--- webproject/accounts/views.py
+++ webproject/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+		else:
+			args = {'form': form, 'invalid': True}
+			return render(request, 'register.html', args)
+	else:
+		form = UserCreationForm()
+
+		args = {'form': form, 'invalid': False}
+		return render(request, 'register.html', args)
