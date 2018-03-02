b3383fccaf173b8a9a1700e6fe4c19f4a9828a3c
heroicstoic <cinnaman.123@gmail.com>
2018-03-02 13:50:22 -0600

    Form styling, certain access only to authenticated

--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog',
     'accounts',
 ]
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "webproject/static"),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
